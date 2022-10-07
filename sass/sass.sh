c6b4a4b60b03538d1407c3415db03748012fc843
Thomas <thomas.lhts.95@icloud.com>
2022-10-07 22:39:43 +0200

    :fix: sass.sh files order

--- sass/sass.sh
+++ sass/sass.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sass --style compressed header.sass ../css/header.css
-sass --style compressed restaurants_menus.sass ../css/restaurants_menus.css
 sass --style compressed main.sass ../css/main.css
 sass --style compressed features.sass ../css/features.css
+sass --style compressed restaurants_menus.sass ../css/restaurants_menus.css
