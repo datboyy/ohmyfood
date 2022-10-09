38f1212a0f2d7a6617799412a741f2399dc392f1
Thomas <thomas.lhts.95@icloud.com>
2022-10-09 19:23:39 +0200

    :fix: shorter sass syntax for header.sass & restaurants_list.sass

--- sass/sass.sh
+++ sass/sass.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sass --style compressed header.sass ../css/header.css
-sass --style compressed main.sass ../css/main.css
-sass --style compressed features.sass ../css/features.css
-sass --style compressed restaurants_menus.sass ../css/restaurants_menus.css
-sass --style compressed restaurants_list.sass ../css/restaurants_list.css
+sass --style compressed main.sass                   ../css/main.css
+sass --style compressed header.sass                 ../css/header.css
+sass --style compressed features.sass               ../css/features.css
+sass --style compressed restaurants_menus.sass      ../css/restaurants_menus.css
+sass --style compressed restaurants_list.sass       ../css/restaurants_list.css
+sass --style compressed footer.sass                 ../css/footer.css
