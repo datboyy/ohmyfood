0fe017a46017a857728a1facc03d00353c302927
Thomas <thomas.lhts.95@icloud.com>
2022-10-09 17:32:08 +0200

    restaurants_list.sass

--- sass/sass.sh
+++ sass/sass.sh
@@ -3,3 +3,4 @@ sass --style compressed header.sass ../css/header.css
 sass --style compressed main.sass ../css/main.css
 sass --style compressed features.sass ../css/features.css
 sass --style compressed restaurants_menus.sass ../css/restaurants_menus.css
+sass --style compressed restaurants_list.sass ../css/restaurants_list.css
