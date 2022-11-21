09a739ecd81e9abfd46f3a1756f60f15e11c5bbe
Thomas LHTS <thomas.lhts.95@icloud.com>
2022-11-21 22:42:25 +0100

    :feat: updated sass.sh & sass.bat to compile sass files

--- sass/sass.bat
+++ sass/sass.bat
@@ -1,10 +1,10 @@
-sass --style compressed overall.sass		    ../css/overall.sass
+sass --style compressed overall.sass		            ../css/overall.sass
 sass --style compressed main.sass                   ../css/main.css
 sass --style compressed header.sass                 ../css/header.css
 sass --style compressed features.sass               ../css/features.css
 sass --style compressed restaurants_menus.sass      ../css/restaurants_menus.css
 sass --style compressed restaurants_list.sass       ../css/restaurants_list.css
-saas --style compressed heart-icon.sass		    ../css/heart-icon.css
-sass --style compressed	loader.sass		          ../css/loader.sass
+sass --style compressed heart-icon.sass		          ../css/heart-icon.css
+sass --style compressed	loader.sass		              ../css/loader.sass
 sass --style compressed footer.sass                 ../css/footer.css
 pause
